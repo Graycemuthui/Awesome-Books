67414aad416a510aa7288ddc61481faa56b080ef
Grayce Muthui <graycemuthui20@gmail.com>
2022-08-16 19:34:05 +0300

    Update selectors.js

--- modules/javascript/selectors.js
+++ modules/javascript/selectors.js
@@ -1,12 +1,10 @@
 export default class selectors {
-  // Let's get the menu elements
   static listElement = document.getElementById('list-menu');
 
   static addElement = document.getElementById('add-menu');
 
   static contactElement = document.getElementById('contact-menu');
 
-  // Let's get the sections elements
   static listSection = document.getElementById('lists');
 
   static addSection = document.getElementById('add-book');
